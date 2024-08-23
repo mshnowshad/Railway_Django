f9205dae116785c5445912635863d21a84c77654
nowshedahmed <[nowshedahmed59@gmail.com]>
2024-08-23 12:05:22 +0600

    inlite3

--- verceldeploy/settings.py
+++ verceldeploy/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'verceldeploy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'verceldb',
-#         'USER': 'default',
-#         'PASSWORD': 'Fx3NXs2HYJTo',
-#         'HOST': 'ep-tight-truth-a4ue91uk-pooler.us-east-1.aws.neon.tech',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'Fx3NXs2HYJTo',
+        'HOST': 'ep-tight-truth-a4ue91uk-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
