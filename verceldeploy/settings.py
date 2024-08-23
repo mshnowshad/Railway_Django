dce0c22b1f6f946c988af0d2955f2a2e23cac007
nowshedahmed <[nowshedahmed59@gmail.com]>
2024-08-23 12:19:10 +0600

    inlite34

--- verceldeploy/settings.py
+++ verceldeploy/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-MEDIA_URL = '/var/task/media'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = '/tmp/media'
+
