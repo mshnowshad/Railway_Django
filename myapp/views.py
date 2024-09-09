f3742c0044f34100dd7dfaf34ffeb24a1516f8c1
nowshedahmed <[nowshedahmed59@gmail.com]>
2024-09-09 18:50:59 +0600

    update all things

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from .models import Names_app
 from .forms import Names_appForm
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
-    prods = Names_app.objects.all()
+    lists = Names_app.objects.all()
 
 
     #add task in list
@@ -13,13 +14,14 @@ def index(request):
         form = Names_appForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Task added successfully!")
             return redirect('index')
     else:
         form = Names_appForm()
 
 
     context = {
-        "prods":prods,
+        "lists":lists,
         "form":form,
     }
     return render(request, 'index.html',context)
